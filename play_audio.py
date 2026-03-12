19d18c49b64560702513236b0fc065ef4aa72ff5
Alisha Joshi <alishajoshi190@gmail.com>
2026-03-12 14:26:32 +0545

    Initial commit: script, audio, Python file, README

---
+++ play_audio.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+
+audio_files = [
+    "rain_audio.mp3",
+    "rain_audio2.mp3",
+    "rain_audio3.mp3",
+    "rain_audio4.mp3",
+    "rain_audio5.mp3"
+]
+for file in audio_files:
+    playsound(file)
